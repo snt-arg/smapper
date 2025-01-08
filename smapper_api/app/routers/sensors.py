3f0a3d0f55cadba9c45c60f3186ecace7f269d00
Pedro Soares <pmbs.123@gmail.com>
2025-01-08 14:38:14 +0100

    feat(api): improve handling of services failure

--- smapper_api/app/routers/sensors.py
+++ smapper_api/app/routers/sensors.py
@@ -1,12 +1,14 @@
+import logging
 from functools import lru_cache
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from app.config.settings import ServicesSettings
-from app.utils.system_services import service_start, service_stop, service_status
+from app.utils.system_services import ServiceStatus, service_start, service_stop, service_status
 
+LOGGER = logging.getLogger('uvicorn.error')
 
-router = APIRouter(prefix="/sensors")
+router = APIRouter(prefix="/api/sensors")
 
 
 @lru_cache
@@ -16,14 +18,19 @@ def get_service_settings():
 
 @router.post("/lidar/start")
 def lidar_start(settings: Annotated[ServicesSettings, Depends(get_service_settings)]):
-    status = service_start(settings.lidar_sensor_service_name)
-    return {"status": status}
+    success = service_start(settings.lidar_sensor_service_name)
 
+    if not success:
+        LOGGER.error("Failed to start LiDAR service. ")
+        raise HTTPException(status_code=503, detail="LiDAR service has failed to start.")
 
 @router.post("/lidar/stop")
 def lidar_stop(settings: Annotated[ServicesSettings, Depends(get_service_settings)]):
-    status = service_stop(settings.lidar_sensor_service_name)
-    return {"status": status}
+    success = service_stop(settings.lidar_sensor_service_name)
+
+    if not success:
+        LOGGER.error("Failed to stop LiDAR service. ")
+        raise HTTPException(status_code=503, detail="LiDAR service has failed to stop.")
 
 
 @router.get("/lidar/status")
@@ -34,14 +41,20 @@ def lidar_status(settings: Annotated[ServicesSettings, Depends(get_service_setti
 
 @router.post("/cameras/start")
 def cameras_start(settings: Annotated[ServicesSettings, Depends(get_service_settings)]):
-    status = service_start(settings.cameras_sensor_service_name)
-    return {"status": status}
+    success = service_start(settings.cameras_sensor_service_name)
+
+    if not success:
+        LOGGER.error("Failed to start camera service.")
+        raise HTTPException(status_code=503, detail="Cameras service has failed to start.")
 
 
 @router.post("/cameras/stop")
 def cameras_stop(settings: Annotated[ServicesSettings, Depends(get_service_settings)]):
-    status = service_stop(settings.cameras_sensor_service_name)
-    return {"status": status}
+    success = service_stop(settings.cameras_sensor_service_name)
+
+    if not success:
+        LOGGER.error("Failed to stop camera service. ")
+        raise HTTPException(status_code=503, detail="Cameras service has failed to stop.")
 
 
 @router.get("/cameras/status")

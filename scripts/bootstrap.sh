13313b8a66aa18dde2b93923e69724cf7c527195
Pedro Soares <pmbs.123@gmail.co>
2025-02-14 11:48:40 +0100

    chore: create empty bootstrap script

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check if command has been executed with sudo
+# Create a list of apt packages to be installed
+# - nvidia-jetpack
+# - nvidia-container
+# APT update
+# Install ros2 isaac version
+# check if docker is installed
+# check if docker is able to run with --gpus
+# Install econsystems kernel drivers -> this reboots the pc, so should be done last
+#   or come up with a different system
+# Install ouster deps

9a263b03109adb6d00f1de37664a19b0086d840c
Pedro Soares <pmbs.123@gmail.co>
2025-02-19 16:00:26 +0100

    feat: simple executable to test multi_camera
    Currently one needs to manually kill the process due to threads not
    being killed

--- software/ros_ws/src/argus_camera_ros/src/single_camera_node.cpp
+++ software/ros_ws/src/argus_camera_ros/src/single_camera_node.cpp
@@ -1,9 +1,28 @@
-#include <rclcpp/logger.hpp>
+#include <unistd.h>
+
+#include <iostream>
 #include <rclcpp/rclcpp.hpp>
+
+#include "argus_camera_ros/multi_camera.hpp"
 
 int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
     RCLCPP_INFO(rclcpp::get_logger("main"), "Hello from single camera node");
+
+    MultiCamera::Config config{
+        .device_ids = {0, 1, 2, 3},
+        .image_width = 640,
+        .image_height = 480,
+        .framerate = 20,
+        .buffer_size = 8,
+    };
+
+    MultiCamera cameras(config);
+    if (cameras.init()) cameras.start_capture();
+
+    cameras.wait();
+    cameras.stop_capture();
+
     rclcpp::shutdown();
     return 0;
 }

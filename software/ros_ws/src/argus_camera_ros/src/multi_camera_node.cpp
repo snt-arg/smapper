946a425a42a628512ad31a73a0d5c6895e6f3440
Pedro Soares <pmbs.123@gmail.co>
2025-02-19 15:59:18 +0100

    chore: remove useless source

--- software/ros_ws/src/argus_camera_ros/src/multi_camera_node.cpp
+++
@@ -1,9 +0,0 @@
-#include <rclcpp/logger.hpp>
-#include <rclcpp/rclcpp.hpp>
-
-int main(int argc, char** argv) {
-    rclcpp::init(argc, argv);
-    RCLCPP_INFO(rclcpp::get_logger("main"), "Hello from multi camera node");
-    rclcpp::shutdown();
-    return 0;
-}

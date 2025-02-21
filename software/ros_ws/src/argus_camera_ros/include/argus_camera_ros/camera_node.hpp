66ab294b3d8fbcb175cfe5b486c37452c5ebc5d7
Pedro Soares <pmbs.123@gmail.co>
2025-02-21 16:27:28 +0100

    feat: add support for camera info

--- software/ros_ws/src/argus_camera_ros/include/argus_camera_ros/camera_node.hpp
+++ software/ros_ws/src/argus_camera_ros/include/argus_camera_ros/camera_node.hpp
@@ -2,9 +2,11 @@
 #define CAMERA_NODE_HPP
 
 #include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/detail/camera_info__struct.hpp>
 #include <sensor_msgs/msg/image.hpp>
 
 #include "argus_camera_ros/multi_camera.hpp"
+#include "camera_info_manager/camera_info_manager.hpp"
 
 class ArgusCameraNode : public rclcpp::Node {
    public:
@@ -12,10 +14,26 @@ class ArgusCameraNode : public rclcpp::Node {
     ~ArgusCameraNode();
 
    private:
-    void pub_callback();
-    std::unique_ptr<MultiCamera> cameras;
-    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr img_pub_;
-    rclcpp::TimerBase::SharedPtr timer_;
+    void pub_callback_();
+    void init_pubs_();
+    void declare_parameters_();
+    void read_parameters_();
+    void build_camera_msg_(std::vector<sensor_msgs::msg::Image::SharedPtr> &msgs,
+                           std::vector<cv::Mat> &camera_frames);
+
+    std::vector<std::string> camera_names_;
+    std::vector<std::string> frame_ids_;
+    std::vector<std::string> camera_urls_;
+    MultiCamera::Config cameras_config_;
+
+    std::unique_ptr<MultiCamera> cameras_;
+
+    std::vector<std::unique_ptr<camera_info_manager::CameraInfoManager>> camera_info_;
+
+    std::vector<rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr> img_pubs_;
+    std::vector<rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr>
+        camera_info_pubs_;
+    rclcpp::TimerBase::SharedPtr pub_timer_;
 };
 
 #endif  // !CAMERA_NODE_HPP
--- software/ros_ws/src/argus_camera_ros/src/argus_camera_ros/camera_node.cpp
+++ software/ros_ws/src/argus_camera_ros/src/argus_camera_ros/camera_node.cpp
@@ -2,39 +2,114 @@
 
 #include <cv_bridge/cv_bridge.h>
 
-#include "argus_camera_ros/multi_camera.hpp"
+#include <camera_info_manager/camera_info_manager.hpp>
+#include <sensor_msgs/msg/detail/camera_info__struct.hpp>
 
-ArgusCameraNode::ArgusCameraNode() : Node("argus_camera_node") {
+static const MultiCamera::Config DEFAULT_CONFIG{
+    .device_ids = {0, 1, 2, 3},
+    .sensor_mode = 2,
+    .image_width = 640,
+    .image_height = 480,
+    .framerate = 20,
+    .buffer_size = 4,
+};
+
+static const std::vector<std::string> DEFAULT_NAMES{"front_left",
+                                                    "front_right",
+                                                    "right",
+                                                    "left"};
+
+static const std::vector<std::string> DEFAULT_FRAME_IDS{"front_left_camera",
+                                                        "front_right_camera",
+                                                        "right_camera",
+                                                        "left_camera"};
+
+static const std::vector<std::string> DEFAULT_URLS{
+    "package://argus_camera_ros/config/front_left_info.yaml",
+    "package://argus_camera_ros/config/front_right_info.yaml",
+    "package://argus_camera_ros/config/right_info.yaml",
+    "package://argus_camera_ros/config/left_info.yaml"};
+
+ArgusCameraNode::ArgusCameraNode() : Node("argus_camera_node"), cameras_config_() {
     RCLCPP_INFO(this->get_logger(), "Hello From CameraNode");
 
-    MultiCamera::Config config{
-        .device_ids = {0, 1, 2, 3},
-        .image_width = 640,
-        .image_height = 480,
-        .framerate = 20,
-        .buffer_size = 8,
-    };
+    declare_parameters_();
+    read_parameters_();
 
-    cameras = std::make_unique<MultiCamera>(config);
+    cameras_ = std::make_unique<MultiCamera>(cameras_config_);
 
-    if (cameras->init()) cameras->start_capture();
+    if (cameras_->init()) cameras_->start_capture();
 
-    img_pub_ = this->create_publisher<sensor_msgs::msg::Image>("/image_raw", 10);
-    timer_ = this->create_wall_timer(std::chrono::milliseconds(1 / config.framerate),
-                                     std::bind(&ArgusCameraNode::pub_callback, this));
+    init_pubs_();
 }
 
 ArgusCameraNode::~ArgusCameraNode() {}
 
-void ArgusCameraNode::pub_callback() {
-    std::vector<cv::Mat> frames;
-    cameras->get_frames(frames);
+void ArgusCameraNode::init_pubs_() {
+    pub_timer_ = this->create_wall_timer(
+        std::chrono::milliseconds(1 / cameras_config_.framerate),
+        std::bind(&ArgusCameraNode::pub_callback_, this));
 
+    for (int i = 0; i < camera_names_.size(); i++) {
+        // INFO: We could perhaps change the queue size, or use a different QoS
+        std::string topic = "/camera/" + camera_names_[i] + "/image_raw";
+        img_pubs_.push_back(this->create_publisher<sensor_msgs::msg::Image>(topic, 10));
+
+        camera_info_pubs_.push_back(
+            this->create_publisher<sensor_msgs::msg::CameraInfo>(
+                camera_names_[i] + "/camera_info", 10));
+
+        // camera_info_manager_ =
+        // std::make_unique<camera_info_manager::CameraInfoManager>(this,
+        // camera_names_[i], camera_urls_[i]);
+
+        camera_info_.push_back(std::make_unique<camera_info_manager::CameraInfoManager>(
+            this, camera_names_[i], camera_urls_[i]));
+    }
+}
+
+void ArgusCameraNode::declare_parameters_() {
+    this->declare_parameter("devices", DEFAULT_CONFIG.device_ids);
+    this->declare_parameter("image_width", DEFAULT_CONFIG.image_width);
+    this->declare_parameter("image_height", DEFAULT_CONFIG.image_height);
+    this->declare_parameter("framerate", DEFAULT_CONFIG.framerate);
+    this->declare_parameter("sensor_mode", DEFAULT_CONFIG.sensor_mode);
+    this->declare_parameter("buffer_size", DEFAULT_CONFIG.buffer_size);
+
+    this->declare_parameter("camera_names", DEFAULT_NAMES);
+    this->declare_parameter("frame_ids", DEFAULT_FRAME_IDS);
+    this->declare_parameter("camera_urls", DEFAULT_URLS);
+}
+
+void ArgusCameraNode::read_parameters_() {
+    this->get_parameter("devices", cameras_config_.device_ids);
+    this->get_parameter("image_width", cameras_config_.image_width);
+    this->get_parameter("image_height", cameras_config_.image_height);
+    this->get_parameter("framerate", cameras_config_.framerate);
+    this->get_parameter("sensor_mode", cameras_config_.sensor_mode);
+    this->get_parameter("buffer_size", cameras_config_.buffer_size);
+
+    this->get_parameter("camera_names", camera_names_);
+    this->get_parameter("frame_ids", frame_ids_);
+    this->get_parameter("camera_urls", camera_urls_);
+}
+
+void ArgusCameraNode::pub_callback_() {
+    std::vector<cv::Mat> frames;
+    if (!cameras_->get_frames(frames)) {
+        RCLCPP_WARN(this->get_logger(), "No Frames to publish");
+        return;
+    }
     std_msgs::msg::Header header;
     header.stamp = this->get_clock()->now();
 
-    sensor_msgs::msg::Image::SharedPtr msg =
-        cv_bridge::CvImage(header, "bgr8", frames[0]).toImageMsg();
+    for (int i = 0; i < frames.size(); i++) {
+        header.frame_id = frame_ids_[i];
+        sensor_msgs::msg::Image::SharedPtr msg =
+            cv_bridge::CvImage(header, "bgr8", frames[i]).toImageMsg();
+        img_pubs_[i]->publish(*msg);
 
-    img_pub_->publish(*msg);
+        sensor_msgs::msg::CameraInfo camera_info = camera_info_[i]->getCameraInfo();
+        camera_info_pubs_[i]->publish(camera_info);
+    }
 }

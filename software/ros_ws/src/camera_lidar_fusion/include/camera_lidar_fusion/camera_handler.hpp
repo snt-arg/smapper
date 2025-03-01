fe8e24a584b4477fde06dcf20a629a3aee2c817c
Pedro Soares <pmbs.123@gmail.com>
2025-03-01 11:34:39 +0000

    chore: ported and refactored into .hpp and .cpp

--- software/ros_ws/src/camera_lidar_fusion/include/camera_lidar_fusion/camera_handler.hpp
+++ software/ros_ws/src/camera_lidar_fusion/include/camera_lidar_fusion/camera_handler.hpp
@@ -2,23 +2,48 @@
 #define CAMERA_HANDLER_HPP
 
 #include <Eigen/Eigen>
+#include <sensor_msgs/msg/camera_info.hpp>
 
 class CameraHandler {
    public:
     CameraHandler();
     ~CameraHandler();
 
+    Eigen::Vector2d project_lidar_to_camera(Eigen::Vector3d point_3d);
+    Eigen::Vector2d pinhole_distort(const Eigen::Vector2d& pt);
+    bool transform_initialized();
+
+    // Setters
+   public:
+    void set_camera_frame_id(std::string frame_id);
+    void set_image_encoding(std::string encoding);
+    void set_camera_info(const sensor_msgs::msg::CameraInfo::ConstSharedPtr& msg);
+    void set_lidar_to_camera_matrix(Eigen::Matrix4d T_lidar_cam);
+    void set_lidar_to_camera_inv_matrix(Eigen::Matrix4d T_lidar_cam_inv);
+    void set_lidar_to_camera_projection_matrix();
+
+    // Getters
+   public:
+    double get_image_height();
+    double get_image_width();
+    std::string get_camera_frame_id();
+    std::string get_image_encoding();
+    Eigen::Matrix4d get_lidar_to_camera_matrix() { return lidar_to_camera_matrix_; }
+    Eigen::Matrix4d get_lidar_to_camera_inv_matrix();
+    Eigen::Matrix<double, 3, 4> get_lidar_to_camera_projection_matrix();
+
    private:
-    int image_width, image_height;
-    Eigen::Matrix<double, 1, 5> distortion_matrix;
-    Eigen::Matrix<double, 3, 4> projection_matrix;
-    Eigen::Matrix<double, 3, 4> lidar_to_camera_projection_matrix;
+    int image_width_, image_height_;
+    Eigen::Matrix3d camera_matrix_;
+    Eigen::Matrix<double, 1, 5> distortion_matrix_;
+    Eigen::Matrix<double, 3, 4> projection_matrix_;
+    Eigen::Matrix<double, 3, 4> lidar_to_camera_projection_matrix_;
 
-    Eigen::Matrix4d lidar_to_camera_matrix, lidar_to_camera_inv_matrix;
+    Eigen::Matrix4d lidar_to_camera_matrix_, lidar_to_camera_inv_matrix_;
 
-    bool is_transform_initialized = false;
-    std::string camera_frame_id;
-    std::string image_encoding;
+    bool is_transform_initialized_ = false;
+    std::string camera_frame_id_;
+    std::string image_encoding_;
 };
 
 #endif  // CAMERA_HANDLER_HPP
---
+++ software/ros_ws/src/camera_lidar_fusion/src/camera_lidar_fusion/camera_handler.cpp
@@ -0,0 +1,103 @@
+#include "camera_lidar_fusion/camera_handler.hpp"
+
+CameraHandler::CameraHandler() {}
+CameraHandler::~CameraHandler() {}
+
+Eigen::Vector2d CameraHandler::project_lidar_to_camera(Eigen::Vector3d point_3d) {
+    const auto pt_2d = (point_3d.template head<2>() / point_3d.z()).eval();
+    const auto pt_d = pinhole_distort(pt_2d);
+
+    const auto& fx = camera_matrix_(0, 0);
+    const auto& fy = camera_matrix_(1, 1);
+    const auto& cx = camera_matrix_(0, 2);
+    const auto& cy = camera_matrix_(1, 2);
+
+    return {fx * pt_d(0) + cx, fy * pt_d(1) + cy};
+}
+
+Eigen::Vector2d CameraHandler::pinhole_distort(const Eigen::Vector2d& pt) {
+    const auto& k1 = distortion_matrix_(0, 0);
+    const auto& k2 = distortion_matrix_(0, 1);
+    const auto& k3 = distortion_matrix_(0, 3);
+
+    const auto& p1 = distortion_matrix_(0, 4);
+    const auto& p2 = distortion_matrix_(0, 2);
+
+    const auto x2 = pt.x() * pt.x();
+    const auto y2 = pt.y() * pt.y();
+
+    const auto r2 = x2 + y2;
+    const auto r4 = r2 * r2;
+    const auto r6 = r2 * r4;
+
+    const auto r_coeff = 1.0 + k1 * r2 + k2 * r4 + k3 * r6;
+    const auto t_coeff1 = 2.0 * pt.x() * pt.y();
+    const auto t_coeff2 = r2 + 2.0 * x2;
+    const auto t_coeff3 = r2 + 2.0 * y2;
+
+    const auto x = r_coeff * pt.x() + p1 * t_coeff1 + p2 * t_coeff2;
+    const auto y = r_coeff * pt.y() + p1 * t_coeff3 + p2 * t_coeff1;
+
+    return Eigen::Vector2d(x, y);
+}
+
+bool CameraHandler::transform_initialized() { return is_transform_initialized_; }
+
+// Setters
+
+void CameraHandler::set_camera_info(
+    const sensor_msgs::msg::CameraInfo::ConstSharedPtr& msg) {
+    image_height_ = msg->height;
+    image_width_ = msg->width;
+
+    camera_matrix_(0, 0) = msg->k[0];
+    camera_matrix_(0, 2) = msg->k[2];
+    camera_matrix_(1, 1) = msg->k[4];
+    camera_matrix_(1, 2) = msg->k[5];
+    camera_matrix_(2, 2) = 1;
+
+    distortion_matrix_(0, 0) = msg->d[0];
+    distortion_matrix_(0, 1) = msg->d[1];
+    distortion_matrix_(0, 2) = msg->d[3];
+    distortion_matrix_(0, 3) = msg->d[4];
+    distortion_matrix_(0, 4) = msg->d[2];
+
+    projection_matrix_(0, 0) = msg->p[0];
+    projection_matrix_(0, 2) = msg->p[2];
+    projection_matrix_(1, 1) = msg->p[5];
+    projection_matrix_(1, 2) = msg->p[6];
+    projection_matrix_(2, 2) = 1;
+}
+void CameraHandler::set_lidar_to_camera_matrix(Eigen::Matrix4d T_lidar_cam) {
+    lidar_to_camera_matrix_ = Eigen::Matrix4d::Identity();
+    lidar_to_camera_matrix_ = T_lidar_cam;
+    is_transform_initialized_ = true;
+}
+
+void CameraHandler::set_lidar_to_camera_inv_matrix(Eigen::Matrix4d T_lidar_cam_inv) {
+    lidar_to_camera_inv_matrix_ = Eigen::Matrix4d::Identity();
+    lidar_to_camera_inv_matrix_ = T_lidar_cam_inv;
+}
+
+void CameraHandler::set_camera_frame_id(std::string frame_id) {
+    camera_frame_id_ = frame_id;
+}
+void CameraHandler::set_image_encoding(std::string encoding) {
+    image_encoding_ = encoding;
+}
+
+// Getters
+
+Eigen::Matrix<double, 3, 4> CameraHandler::get_lidar_to_camera_projection_matrix() {
+    return lidar_to_camera_projection_matrix_;
+}
+Eigen::Matrix4d CameraHandler::get_lidar_to_camera_inv_matrix() {
+    return lidar_to_camera_inv_matrix_;
+}
+
+double CameraHandler::get_image_height() { return image_height_; }
+double CameraHandler::get_image_width() { return image_width_; }
+
+std::string CameraHandler::get_camera_frame_id() { return camera_frame_id_; }
+
+std::string CameraHandler::get_image_encoding() { return image_encoding_; }

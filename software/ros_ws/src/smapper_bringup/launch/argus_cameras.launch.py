14f2c71018498471dcf56157dcc6355f862a0ff0
Pedro Soares <pmbs.123@gmail.com>
2025-06-06 14:50:16 +0200

    chore: use newer values for transformation between cameras and lidar

--- software/ros_ws/src/smapper_bringup/launch/argus_cameras.launch.py
+++ software/ros_ws/src/smapper_bringup/launch/argus_cameras.launch.py
@@ -28,117 +28,129 @@ def launch_argus_cameras(ld: LaunchDescription) -> None:
 
 
 def publish_tfs(ld: LaunchDescription) -> None:
-    ld.add_action(
-        Node(
-            package="tf2_ros",
-            executable="static_transform_publisher",
-            name="static_transform_publisher_os_fr_camera",
-            arguments=[
-                "--x",
-                "-0.012168688414324998",
-                "--y",
-                "-0.05230268675672892",
-                "--z",
-                "-0.04846000514448317",
-                "--qx",
-                "0.3586529704455553",
-                "--qy",
-                "0.6153675089165976",
-                "--qz",
-                "-0.607803551863725",
-                "--qw",
-                "-0.351092178924853",
-                "--frame-id",
-                "os_lidar",
-                "--child-frame-id",
-                "front_left_camera",
-            ],
-        )
-    )
+    # ld.add_action(
+    #     Node(
+    #         package="tf2_ros",
+    #         executable="static_transform_publisher",
+    #         name="static_transform_publisher_os_fr_camera",
+    #         arguments=[
+    #             "--x",
+    #             "-0.012168688414324998",
+    #             "--y",
+    #             "-0.05230268675672892",
+    #             "--z",
+    #             "-0.04846000514448317",
+    #             "--qx",
+    #             "0.3586529704455553",
+    #             "--qy",
+    #             "0.6153675089165976",
+    #             "--qz",
+    #             "-0.607803551863725",
+    #             "--qw",
+    #             "-0.351092178924853",
+    #             "--frame-id",
+    #             "os_lidar",
+    #             "--child-frame-id",
+    #             "front_left_camera",
+    #         ],
+    #     )
+    # )
+    #
+    # ld.add_action(
+    #     Node(
+    #         package="tf2_ros",
+    #         executable="static_transform_publisher",
+    #         name="static_transform_publisher_os_fr_camera",
+    #         arguments=[
+    #             "--x",
+    #             "-0.007861905010314724",
+    #             "--y",
+    #             "0.012601838083764312",
+    #             "--z",
+    #             "-0.04350629916231412",
+    #             "--qx",
+    #             "-0.6120399884004516",
+    #             "--qy",
+    #             "-0.3606205347019306",
+    #             "--qz",
+    #             "0.3508780584930321",
+    #             "--qw",
+    #             "0.6101184070475415",
+    #             "--frame-id",
+    #             "os_lidar",
+    #             "--child-frame-id",
+    #             "front_right_camera",
+    #         ],
+    #     )
+    # )
+    #
+    # ld.add_action(
+    #     Node(
+    #         package="tf2_ros",
+    #         executable="static_transform_publisher",
+    #         name="static_transform_publisher_os_fr_camera",
+    #         arguments=[
+    #             "--x",
+    #             "0.07974294467004457",
+    #             "--y",
+    #             "0.031088269481674376",
+    #             "--z",
+    #             "-0.0064455203636650015",
+    #             "--qx",
+    #             "-0.002113388348180505",
+    #             "--qy",
+    #             "0.7245350428347996",
+    #             "--qz",
+    #             "-0.6892338777765248",
+    #             "--qw",
+    #             "0.0010802866284276606",
+    #             "--frame-id",
+    #             "os_lidar",
+    #             "--child-frame-id",
+    #             "side_left_camera",
+    #         ],
+    #     )
+    # )
 
     ld.add_action(
         Node(
             package="tf2_ros",
             executable="static_transform_publisher",
-            name="static_transform_publisher_os_fr_camera",
+            name="static_transform_publisher_imu_to_camera",
             arguments=[
-                "--x",
-                "-0.007861905010314724",
-                "--y",
-                "0.012601838083764312",
-                "--z",
-                "-0.04350629916231412",
-                "--qx",
-                "-0.6120399884004516",
-                "--qy",
-                "-0.3606205347019306",
-                "--qz",
-                "0.3508780584930321",
-                "--qw",
-                "0.6101184070475415",
-                "--frame-id",
-                "os_lidar",
-                "--child-frame-id",
-                "front_right_camera",
+                "--x", "0.05734822052548241",
+                "--y", "0.05973053014042769",
+                "--z", "-0.04733641790040566",
+                "--qx", "0.6203218624628579",
+                "--qy", "-0.35708040212980113",
+                "--qz", "0.35693510550626295",
+                "--qw", "-0.60024303729628",
+                "--frame-id", "os_imu",
+                "--child-frame-id", "front_left",
             ],
         )
     )
 
     ld.add_action(
-        Node(
-            package="tf2_ros",
-            executable="static_transform_publisher",
-            name="static_transform_publisher_os_fr_camera",
-            arguments=[
-                "--x",
-                "0.07974294467004457",
-                "--y",
-                "0.031088269481674376",
-                "--z",
-                "-0.0064455203636650015",
-                "--qx",
-                "-0.002113388348180505",
-                "--qy",
-                "0.7245350428347996",
-                "--qz",
-                "-0.6892338777765248",
-                "--qw",
-                "0.0010802866284276606",
-                "--frame-id",
-                "os_lidar",
-                "--child-frame-id",
-                "side_left_camera",
-            ],
-        )
+    Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_transform_publisher_imu_to_camera",
+        arguments=[
+            "--x", "0.03813065464667919",
+            "--y", "-0.047371485526111944",
+            "--z", "-0.036135738068488736",
+            "--qx", "0.364039840144593",
+            "--qy", "-0.6084624915794161",
+            "--qz", "0.6125602747186555",
+            "--qw", "-0.34931118070435924",
+            "--frame-id", "os_imu",
+            "--child-frame-id", "front_right",
+        ],
     )
+)
 
-    ld.add_action(
-        Node(
-            package="tf2_ros",
-            executable="static_transform_publisher",
-            name="static_transform_publisher_os_fr_camera",
-            arguments=[
-                "--x",
-                "0.029100216695835537",
-                "--y",
-                "-0.06844003776442746",
-                "--z",
-                "-0.03425028075003019",
-                "--qx",
-                "-0.7031074296764369",
-                "--qy",
-                "0.013863898949048182",
-                "--qz",
-                "-0.013130945492662398",
-                "--qw",
-                "0.7108272032710857",
-                "--frame-id",
-                "os_lidar",
-                "--child-frame-id",
-                "side_right_camera",
-            ],
-        )
-    )
+
 
 
 def generate_launch_description() -> LaunchDescription:

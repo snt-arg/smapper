d973bb50e3b4adf8446e47c93b7e15a1834ad5bf
Pedro Soares <pmbs.123@gmail.com>
2025-07-20 14:35:32 +0200

    feat(script): add commands for ros2 installation

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -16,9 +16,32 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install -y nvidia-jetpack
 
-sudo apt install -y python3-pip
+sudo apt install -y python3-pip python3-venv
 sudo pip3 install jetson-stats
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
+
+# Install ros2 humble
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+sudo apt update && sudo apt install curl -y
+export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
+curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $VERSION_CODENAME)_all.deb" # If using Ubuntu derivates use $UBUNTU_CODENAME
+sudo dpkg -i /tmp/ros2-apt-source.deb
+
+sudo apt update
+
+sudo apt install ros-humble-desktop
+sudo apt install ros-dev-tools
+
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+echo "source ~/smapper/software/ros_ws/install/setup.bash" >> ~/.bashrc
+
+sudo apt install ros-humble-rosbag2-storage-mcap
+
+
+# UV astral package manager
+curl -LsSf https://astral.sh/uv/install.sh | sh

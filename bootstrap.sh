c7d461a3d3b70629ba6bd21aaf8621f6e3e9b429
Pedro Soares <pmbs.123@gmail.com>
2025-08-05 10:32:07 +0200

    feat: complete bootstrap by executing functions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# This is an experimental bootstrap script. It has never been executed on the
+# real platform, therefore changes of it not fully working are high.
+
 SMAPPER_REPO_PATH=$HOME/smapper
 SMAPPER_APP_PATH=$SMAPPER_REPO_PATH/software/smapper_app
 ROS_WS_PATH=$SMAPPER_REPO_PATH/software/ros_ws
+
+PATCHES_PATH=$SMAPPER_REPO_PATH/infra/patches
+PATCHES_ZIP_NAME=5.15.136-tegra.tar.xz
 
 API_URL="http://10.42.0.1:8000/api/v1"
 ROSBOARD_URL="http://10.42.0.1:8888"
@@ -182,9 +188,14 @@ deploy_smapper_app(){
 }
 
 clone_smapper_repo(){
-    log_info "Deploying SMapper APP..."
+    log_info "Clonning smapper repository..."
 
-    git clone --recurse-submodules https://github.com/snt-arg/smapper.git $SMAPPER_REPO_PATH
+    if [[ ! -d $SMAPPER_REPO_PATH ]] then
+        git clone --recurse-submodules https://github.com/snt-arg/smapper.git $SMAPPER_REPO_PATH
+    else
+        log_info "smapper repository is already available! Pulling latests changes"
+        cd $SMAPPER_REPO_PATH && git pull
+    fi
 
     log_info "Done!"
 }
@@ -192,7 +203,33 @@ clone_smapper_repo(){
 realsense_support(){
     log_info "Adding Support to Realsense..."
 
-    log_error "Not Yet Implemented"
+    log_info "Installing Dependencies"
+    sudo apt-get install git libssl-dev libusb-1.0-0-dev libudev-dev pkg-config libgtk-3-dev -y
+
+    if [[ ! -d ~/tools ]] then
+        mkdir ~/tools
+    fi
+    pushd ~/tools
+
+    log_info "Clonning librealsense into ~/tools/librealsense"
+    git clone -b v2.56.4 https://github.com/IntelRealSense/librealsense.git
+    cd librealsense
+
+    # INFO: In case the realsense is not detected, the kernel was not successfully
+    # patched with apply_patches(). If that is the case you can run the following command:
+    # ./scripts/patch-realsense-ubuntu-L4T.sh
+
+    log_info "Applying realsense udev rules"
+    ./scripts/setup_udev_rules.sh
+
+    log_info "Building librealsense"
+    mkdir build && cd build
+    cmake .. -DBUILD_EXAMPLES=true -DCMAKE_BUILD_TYPE=release \
+        -DFORCE_RSUSB_BACKEND=false -DBUILD_WITH_CUDA=true && \
+        make -j$(($(nproc)-1)) && \
+        sudo make install
+
+    popd
 
     log_info "Done!"
 }
@@ -200,18 +237,25 @@ realsense_support(){
 apply_patches(){
     log_info "Applying Kernel Module Patches..."
 
-    log_error "Not Yet Implemented"
     # unzip from infra/patches and cp to /lib/modules
+    pushd $PATCHES_PATH
+
+    tar -xf $PATCHES_ZIP_NAME
+    cd 5.15.136-tegra
+
+    sudo cp -r 5.15.136-tegra /lib/modules/
+
+    popd
 
     log_info "Done!"
 }
 
 build_rosws(){
-    log_info "Applying Kernel Module Patches..."
+    log_info "Building ROS workspace..."
 
     pushd ROS_WS_PATH > /dev/null
 
-    rosdep install --from-paths --ignore-src -r
+    rosdep install -i --from-path src --skip-keys=librealsense2 -y
     colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
 
     popd > /dev/null
@@ -220,11 +264,53 @@ build_rosws(){
 }
 
 install_mprocs(){
-    log_info "Applying Kernel Module Patches..."
+    log_info "Installing mprocs..."
 
-    log_error "Not Yet Implemented"
+    curl -L -o mprocs.tar.gz https://github.com/pvolok/mprocs/releases/download/v0.7.3/mprocs-0.7.3-linux-aarch64-musl.tar.gz
+
+    if [[ -f mprocs.tar.gz ]] then
+        log_info "Copying binary into ~/.local/bin"
+        tar -xf mprocs.tar.gz && mv mprocs ~/.local/bin
+        rm mprocs.tar.gz
+        echo "alias smapper=mprocs -c $SMAPPER_REPO_PATH/config/mprocs.yaml" >> ~/.bashrc
+    fi
 
     log_info "Done!"
 }
 
-# echo "alias smapper=mprocs -c /home/smapper/smapper/config/mprocs.yaml" >> ~/.bashrc
+
+log_warning "This script has never been executed on the real platform. It is very experimental!"
+
+kernel_version=$(uname -r)
+
+if [[ kernel_version != "5.15.136-tegra" ]] then
+    log_error "This script is made to work with Jetpack 6.0"
+    exit 1
+fi
+
+log_warning "Bootstrapping is about to begin."
+log_warning "You will need to enter sudo password. Be vigilant!"
+sleep 2
+
+clone_smapper_repo
+
+update_system
+
+install_dependencies
+install_ros2
+install_ros_deps
+install_mprocs
+
+configure_docker
+
+apply_patches
+setup_network
+
+realsense_support
+build_rosws
+
+deploy_smapper_api
+deploy_smapper_app
+
+log_info "Bootstrap is concluded. Be sure to reboot computer!"
+sleep 2
